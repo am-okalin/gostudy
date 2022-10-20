e41e13708fa550f62da4a0d38e095254bf19de98
am-okalin <928622276@qq.com>
2022-10-20 18:08:40 +0800

    feat: 添加模板渲染

---
+++ gopkg/template/rand.go
@@ -0,0 +1,45 @@
+package template
+
+import (
+	"fmt"
+	"github.com/prometheus/common/log"
+	"reflect"
+	"strings"
+)
+
+const TagName = "template"
+
+type Inventory struct {
+	ShopName string `template:"STORE_NAME"`
+	Count    uint
+}
+
+func getKey(t reflect.StructField, tagName string) string {
+	tag := t.Tag.Get(tagName)
+	if tag == "" {
+		return t.Name
+	}
+	return tag
+}
+
+// Render 从结构体渲染到变量
+func Render(str string, variables interface{}, left, right string) string {
+	t, v := reflect.TypeOf(variables), reflect.ValueOf(variables)
+	if v.Elem().Kind() == reflect.Struct {
+		t, v = t.Elem(), v.Elem()
+	}
+
+	if v.Kind() != reflect.Struct {
+		err := fmt.Errorf("ToMap only accepts struct or struct pointer; got %T", v)
+		log.Error(err)
+		return str
+	}
+
+	for i := 0; i < t.NumField(); i++ {
+		key := fmt.Sprintf("%s%s%s", left, getKey(t.Field(i), TagName), right)
+		val := fmt.Sprintf("%v", v.Field(i).Interface())
+		str = strings.Replace(str, key, val, -1)
+	}
+
+	return str
+}
--- gopkg/template/template_test.go
+++ gopkg/template/template_test.go
@@ -6,11 +6,6 @@ import (
 	"text/template"
 )
 
-type Inventory struct {
-	ShopName string `template:"STORE_NAME"`
-	Count    uint
-}
-
 func Test1(t *testing.T) {
 	sweaters := Inventory{"wool", 17}
 	tmpl, err := template.New("test").Delims("*|", "|*").
@@ -24,4 +19,13 @@ func Test1(t *testing.T) {
 		panic(err)
 	}
 	t.Log(buf.String())
+}
+
+func TestRender(t *testing.T) {
+	str := "*|SHOP_DOMAIN|*/discount/*|INVITEE_CODE|*"
+	str = Render(str, &Inventory{
+		ShopName: "wtn_shop_domain",
+		Count:    10,
+	}, "*|", "|*")
+	t.Log(str)
 }
