c598d33a9b9828cc7b35873ec66aaac60e03b68a
downing <downing@fstln.io>
2024-12-27 10:56:00 +0800

    feat:新增rate测试方法

--- pkg_gox/go.mod
+++ pkg_gox/go.mod
@@ -5,4 +5,5 @@ go 1.18
 require (
 	github.com/pkg/errors v0.9.1
 	golang.org/x/text v0.21.0
+	golang.org/x/time v0.8.0
 )
--- pkg_gox/time/rate/rate_test.go
+++ pkg_gox/time/rate/rate_test.go
@@ -1,1 +1,51 @@
 package rate
+
+import (
+	"fmt"
+	"golang.org/x/time/rate"
+	"testing"
+	"time"
+)
+
+// 速率限制其
+func TestLimiter(t *testing.T) {
+	// 创建一个速率限制器，每3秒创建1个令牌, 最多3个令牌
+	limiter := rate.NewLimiter(1.0/3.0, 3)
+
+	for i := 0; i < 10; i++ {
+		if limiter.Allow() {
+			t.Log("success", i)
+		} else {
+			t.Log("failed", i)
+		}
+		time.Sleep(time.Second) // 控制循环速度
+	}
+
+	// 等待10秒，恢复至最多3个令牌
+	time.Sleep(10 * time.Second)
+
+	for i := 0; i < 10; i++ {
+		if limiter.Allow() {
+			t.Log("success", i)
+		} else {
+			t.Log("failed", i)
+		}
+		time.Sleep(time.Second) // 控制循环速度
+	}
+}
+
+// 每N次/每个时间间隔 执行操作
+func TestSometime(t *testing.T) {
+	s := &rate.Sometimes{
+		First:    5,               // 第5次执行
+		Every:    2,               // 每2次执行一次
+		Interval: 1 * time.Second, // 时间间隔为1秒, 每隔1秒执行一次
+	}
+
+	for i := 0; i < 100; i++ {
+		s.Do(func() {
+			fmt.Println("Executing operation at", time.Now())
+		})
+		time.Sleep(300 * time.Millisecond) // 控制循环速度
+	}
+}
